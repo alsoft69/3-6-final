dfdceda4989010d55113d7d6121d0f970bdacc47
alsoft69 <alex.eliseev.ae@gmail.com>
2022-01-25 17:48:17 +1200

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,19 @@
+"""
+Модуль по заданию 3-6-9.
+Поиск кнопки на странице с передачей параметров языка
+"""
+import time
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_find_button_basket(browser):
+    """
+    Поиск кнопки "Добавить в корзину"
+    """
+    browser.get(link)
+    time.sleep(10)
+    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(button) > 0, 'Кнопка не найдена!'
